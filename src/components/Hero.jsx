1e89d60aaecfa6413ee4b1a1e34f5fd8c394c87a
Juan Esteban Camargo <juanescacha@hotmail.com>
2023-03-18 19:59:15 -0500

    Hero Model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,13 +18,30 @@ const Hero = () => {
 					</h1>
 					<p
 						className={`${styles.heroSubText} mt-2 max-w-xl text-white`}>
-						I Develop modern, responsive
-						<br className="sm:block hidden" />
-						and accessible web applications
+						I develop modern, responsive
+						<br className="sm:block hidden" /> and accessible web
+						applications
 					</p>
 				</div>
 			</div>
+
 			<ComputersCanvas />
+
+			<div className="absolute bottom-32 xs:bottom-10 w-full flex justify-center">
+				<a href="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary items-start p-2">
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
+			</div>
 		</section>
 	)
 }
